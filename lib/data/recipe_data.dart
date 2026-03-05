e4059c03330a7b3e0413e70f360f27ed5c86850c
Jaden Woody <jwoody6@student.gsu.edu>
2026-03-04 22:31:42 -0500

    added recipes

--- lib/data/recipe_data.dart
+++ lib/data/recipe_data.dart
@@ -3,7 +3,7 @@ import '../models/recipe.dart';
 final List<Recipe> sampleRecipes = [
   Recipe(
     name: 'Spaghetti Bolognese',
-    imagePath: 'assets/images/pasta.jpg',
+    imagePath: 'assets/images/spaghetti_bolognese.png',
     ingredients: [
       'Spaghetti',
       'Ground beef',
@@ -14,5 +14,43 @@ final List<Recipe> sampleRecipes = [
     instructions:
         'Cook pasta. Brown beef with onion & garlic. Add sauce. Combine & serve.',
   ),
-  // Add 3+ more recipes…
+  Recipe(
+    name: 'Cheese Burger',
+    imagePath: 'assets/images/burger.png',
+    ingredients: [
+      'Sesame Seed Bun',
+      'Ground beef',
+      'Ketchp',
+      'Lettuce',
+      'Tomato',
+      'Cheddar Cheese',
+      'Steak Seasoning'
+    ],
+    instructions:
+        'Season beef. Cook until beeg gives clear bubbles (minimal flipping). Add cheese. Let sit. Add to bun with toppings.',
+  ),
+  Recipe(
+    name: 'Pancakes',
+    imagePath: 'assets/images/pancakes.png',
+    ingredients: [
+      'Butter milk pancake mix',
+      'Milk',
+      'Olive oil',
+    ],
+    instructions:
+        'Blend mix and milk. Grease pan with oil on medium-low heat. Pour. Let batter cook until bubling and semi-dry. Flip. Remove from pan.',
+  ),
+  Recipe(
+    name: 'Chicken Tenders',
+    imagePath: 'assets/images/chicken_tenders.png',
+    ingredients: [
+      'Chicken tenderloins',
+      'Flour',
+      'Buttermilk',
+      'Seasoning salt',
+      'Honey Mustard',
+    ],
+    instructions:
+        'Season flour. Coat chicken in flour. Coat chicken in buttermilk. Fry on 350 F for 5 - 7 minutes. Remove and let cool.',
+  ),
 ];
